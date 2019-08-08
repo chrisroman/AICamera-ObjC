796b29b26d81216e6a7decb9a68198e09cde2f42
Christopher Roman <chrisroman@fb.com>
2019-08-08 01:49:45 -0700

    Added code coverage code/configs and removed lib/include directories

--- AICamera-ObjC/AICamera-ObjC/AppDelegate.m
+++ AICamera-ObjC/AICamera-ObjC/AppDelegate.m
@@ -38,10 +38,13 @@
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
-
+#if defined(ENABLE_COVERAGE_PROFILING)
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  extern int __llvm_profile_dump(void);
+  NSLog(@"applicationWillTerminate invoked");
+  (void)__llvm_profile_dump();
 }
+#endif
 
 
 @end
--- AICamera-ObjC/AICamera-ObjC/main.m
+++ AICamera-ObjC/AICamera-ObjC/main.m
@@ -7,6 +7,21 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+#if defined(ENABLE_COVERAGE_PROFILING)
+      {
+        extern void __llvm_profile_set_filename(char *filenamep);
+
+        NSArray *myPathList = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSLog(@"Document(s) paths: %@", myPathList);
+
+        NSString *myPath    = [myPathList  objectAtIndex:0];
+
+        NSString *profile = [NSString stringWithFormat:@"%@/profile.raw", myPath];
+        NSLog(@"Output profile into: %@", profile);
+
+        __llvm_profile_set_filename((char*)[profile UTF8String]);
+      }
+#endif
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
